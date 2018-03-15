200c4941e5761c8ad58b10ab694fb67d5afdbbf7
RomainDW <romain.ollier34@gmail.com>
2018-03-15 19:09:10 +0100

    Création du controller qui gere les pages d'admin

---
+++ src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function dashboardAction(Request $request)
+    {
+        return $this->render('back/dashboard.html.twig');
+    }
+}
