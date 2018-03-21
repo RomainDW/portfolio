af680034120266d7156590280b6ff770808b852e
RomainDW <romain.ollier34@gmail.com>
2018-03-21 12:36:53 +0100

    Script pour la pagination en Ajax

---
+++ web/js/pagination.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function(){
+
+    moove();
+
+    $('#toMoove').remove();
+
+    $(".page-link").click(function (e) {
+        $.ajax({
+            url: $(this).attr('href'),
+            type: 'post'
+        })
+            .done(function(data) {
+                $('#portfolio-container').html(data);
+                moove();
+            });
+        e.preventDefault();
+    })
+});
+
+
+function moove () {
+    $('#modals').html(($('#toMoove').html()));
+}
+
