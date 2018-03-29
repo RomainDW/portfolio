45c73506a57d77f61e0de779ee0e526a0eac584c
RomainDW <romain.ollier34@gmail.com>
2018-03-29 07:12:57 +0200

    Ajout de tiny MCE

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new Stfalcon\Bundle\TinymceBundle\StfalconTinymceBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
