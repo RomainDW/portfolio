880a9cb35c5fea4055c494e980113fc106af14fc
RomainDW <romain.ollier34@gmail.com>
2018-04-11 11:55:42 +0200

    Ajout securityController

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Form\LoginType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login-to-admin", name="security_login")
+     */
+    public function loginAction(Request $request, AuthenticationUtils $authenticationUtils)
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        $form = $this->createForm(LoginType::class, [
+            '_username' => $lastUsername
+        ]);
+
+        return $this->render('back/login.html.twig', array(
+            'form'      => $form->createView(),
+            'error'     => $error,
+        ));
+    }
+
+    /**
+     * @Route("/logout", name="security_logout")
+     */
+    public function logoutAction()
+    {
+        throw new \Exception('this should not be reached!');
+    }
+}
