5771e299cefecd155f1cdecee2af5059dd37beff
RomainDW <romain.ollier34@gmail.com>
2018-04-11 11:50:54 +0200

    Ajout de SnappyBundle pour pdf

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,8 @@ class AppKernel extends Kernel
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
             new Stfalcon\Bundle\TinymceBundle\StfalconTinymceBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
