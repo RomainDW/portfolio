a67bc5ee16cdb50572143ab4302d7c8ce850abf3
RomainDW <romain.ollier34@gmail.com>
2018-04-11 11:55:01 +0200

    ajout des reseaux sociaux

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -3,14 +3,16 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\About;
-use AppBundle\Entity\Category;
 use AppBundle\Entity\Project;
 use AppBundle\Form\AboutType;
+use AppBundle\Form\TweeterType;
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
+use Facebook\Facebook;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class AdminController extends Controller
 {
@@ -19,7 +21,6 @@ class AdminController extends Controller
      */
     public function dashboardAction(Request $request)
     {
-
         $projectData = $this->get('doctrine')
             ->getRepository(Project::class)
             ->findAll();
@@ -28,10 +29,50 @@ class AdminController extends Controller
             ->getRepository(About::class)
             ->findAll();
 
+        $form = $this->createForm(TweeterType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->get('app_core.twitter')->tweeter($form->getData()['tweet']);
+            $this->addFlash('notice', 'Votre tweet a bien été envoyé !');
+            return $this->redirect($request->getUri());
+        }
+
+        $twitterData = $this->get('app_core.twitter')->twitterData();
+
+        $fb = new Facebook([
+            'app_id'        => '444713825964663',
+            'app_secret'    => '53e84c8c95ab5fcf05382d2692de8468',
+            'default_graph_version' => 'v2.10',
+        ]);
+
+        try {
+            // Get the \Facebook\GraphNodes\GraphUser object for the current user.
+            // If you provided a 'default_access_token', the '{access-token}' is optional.
+            $response = $fb->get('/me?fields=friends,feed', 'EAAGUdwGfkncBAHZBe5enN2ar59FCaQslQHKVSaWGtCtwngwbJn7lR1H6X9qhiwPLVkYMzm7Vif14ZAqr6KhaFELKDRZAqrh2e3Y894oWtw7Eia2SKCIN1obztzagRqf2i2JB38JgUpu4rKECJaVgKqr1mNapcgADnqN0SFps5pivfLr2cZA69w7JUNTZBBwcjhRjmrnrfdwZDZD');
+        } catch(FacebookResponseException $e) {
+            // When Graph returns an error
+            echo 'Graph returned an error: ' . $e->getMessage();
+            exit;
+        } catch(FacebookSDKException $e) {
+            // When validation fails or other local issues
+            echo 'Facebook SDK returned an error: ' . $e->getMessage();
+            exit;
+        }
+
+        $me = $response->getGraphUser();
+
+        $numberOfFriends = $me->getField('friends')->getTotalCount();
+        $numberOfFeed = count($me->getField('feed')->getFieldNames());
 
         return $this->render('back/dashboard.html.twig', [
             'projects'      => $projectData,
             'abouts'        => $about,
+            'form'          => $form->createView(),
+            'twitter'       => $twitterData,
+            'nbrFriends'    => $numberOfFriends,
+            'nbrFeed'       => $numberOfFeed
         ]);
     }
 
@@ -62,34 +103,4 @@ class AdminController extends Controller
             'about_form' => $editForm->createView()
         ]);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
